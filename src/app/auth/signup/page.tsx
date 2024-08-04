4cf6bae83f5d50d9b7398c445c3c9b5aaf4bdd41
WebRizen <webrizen@gmail.com>
2024-08-04 16:12:53 +0530

    "Added @radix-ui/react-label and react-icons dependencies to package.json and updated package-lock.json accordingly"

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,144 @@
+"use client";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { FcGoogle } from "react-icons/fc";
+
+export default function page({ className, ...props }: any) {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onSubmit = async (event: any) => {
+        event.preventDefault();
+        setIsLoading(true);
+    };
+
+    const handleGoogleSignIn = async () => {
+        setIsLoading(true);
+
+    };
+
+    return (
+        <div className="w-full flex flex-col gap-2">
+            <div className="flex flex-col space-y-2 text-center mb-4">
+                <h1 className="text-2xl font-semibold tracking-tight">
+                    Create an account
+                </h1>
+                <p className="text-sm text-muted-foreground">
+                    Enter your email below to create your account
+                </p>
+            </div>
+            <div className={cn("grid gap-6", className)} {...props}>
+                <form onSubmit={onSubmit}>
+                    <div className="grid gap-2">
+                        <div className="grid gap-1">
+                            <Label className="sr-only" htmlFor="Username">
+                                Username
+                            </Label>
+                            <Input
+                                id="fullName"
+                                placeholder="Username"
+                                type="text"
+                                autoCapitalize="none"
+                                autoComplete="text"
+                                autoCorrect="off"
+                                disabled={isLoading}
+                            />
+                            <Label className="sr-only" htmlFor="email">
+                                Email
+                            </Label>
+                            <Input
+                                id="email"
+                                placeholder="Email"
+                                type="email"
+                                autoCapitalize="none"
+                                autoComplete="email"
+                                autoCorrect="off"
+                                disabled={isLoading}
+                            />
+                            <Label className="sr-only" htmlFor="password">
+                                Password
+                            </Label>
+                            <Input
+                                id="password"
+                                placeholder="Password"
+                                type="password"
+                                autoCapitalize="none"
+                                autoComplete="password"
+                                autoCorrect="off"
+                                disabled={isLoading}
+                            />
+                        </div>
+                        <Button disabled={isLoading}>
+                            {isLoading && (
+                                <svg
+                                    className="animate-spin h-4 w-4 mr-2 text-red-500"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <circle
+                                        className="opacity-25"
+                                        cx="12"
+                                        cy="12"
+                                        r="10"
+                                        stroke="currentColor"
+                                        strokeWidth="4"
+                                    ></circle>
+                                    <path
+                                        className="opacity-75"
+                                        fill="currentColor"
+                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                    ></path>
+                                </svg>
+                            )}
+                            Sign Up with Email
+                        </Button>
+                    </div>
+                </form>
+                <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                        <span className="w-full border-t" />
+                    </div>
+                    <div className="relative flex justify-center text-xs uppercase">
+                        <span className="bg-white dark:bg-slate-950 rounded-full px-2 text-muted-foreground">
+                            Or continue with
+                        </span>
+                    </div>
+                </div>
+                <Button
+                    variant="outline"
+                    type="button"
+                    disabled={isLoading}
+                    onClick={handleGoogleSignIn}
+                >
+                    {isLoading && (
+                        <svg
+                            className="animate-spin h-4 w-4 mr-2 text-red-500"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <circle
+                                className="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                            ></circle>
+                            <path
+                                className="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                        </svg>
+                    )}
+                    <FcGoogle className="mr-2 h-4 w-4" />
+                    Google
+                </Button>
+            </div>
+        </div>
+    );
+}
